1b48deb75ecf38f18d3b98d90c593eb9b61f1ab7
inar_aman <inaramangeldi54@gmail.com>
2025-01-23 15:44:43 +0500

    Initial commit

---
+++ src/main/java/com/example/salon_project/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.salon_project.repository;
+
+import com.example.salon_project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
